cbb0ee02a9284c4d280c6dc280b97a53902c1678
ShiroOYuki <63907529+ShiroOYuki@users.noreply.github.com>
2023-08-24 18:17:20 +0800

    update

--- ShiroTools/line_stickers/views.py
+++ ShiroTools/line_stickers/views.py
@@ -1,15 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from .download_stickers import Sticker
+import json
 
 # Create your views here.
 
-def line_stickers_mainpage(request):
-    return render(
+def createRecents(request, resp_url, name, url, context={}):
+    if request.COOKIES.get("history"):
+        hist = json.loads(request.COOKIES["history"])
+    else:
+        hist = []
+    if name != "Dashboard" and (len(hist) == 0 or hist[0][0]) != name:
+        hist.insert(0, [name, url])
+    if len(hist) > 20:
+        hist = hist[:20]
+    context["history"] = hist
+    resp = render(
         request, 
-        "line_stickers.html", 
-        context = {"stickers": []}
+        resp_url,
+        context=context
     )
+    resp.set_cookie("history", json.dumps(hist))
+    return resp
+
+def line_stickers_mainpage(request):
+    return createRecents(request, "line_stickers.html", "Line Stickers", "/line_stickers", context={})
 
 def get_stickers(request):
     if request.method == "POST":
