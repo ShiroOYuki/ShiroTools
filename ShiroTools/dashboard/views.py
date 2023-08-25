b08aee055cd4156b2166509fde04f9ad7d2ae0fb
ShiroOYuki <63907529+ShiroOYuki@users.noreply.github.com>
2023-08-25 10:58:03 +0800

    update announcement

--- ShiroTools/dashboard/views.py
+++ ShiroTools/dashboard/views.py
@@ -22,11 +22,15 @@ def createRecents(request, resp_url, name, url, context={}):
     return resp
 
 def index(request):
+    context = {}
     with open("static/dashboard/update.txt", "r", encoding="utf-8") as f:
         logs = [line.strip("\n") for line in f.readlines()]
         context = {
             "logs": logs
         }
+    with open("static/dashboard/announcement.txt", "r", encoding="utf-8") as f:
+        announcement = [line.strip("\n") for line in f.readlines()]
+        context["announcement"] = announcement
     resp = createRecents(request, "dashboard.html", "Dashboard", "/", context)
     return resp
 
