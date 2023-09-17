2128fb6ca9b21298d28342749b3ad57c3ced3252
ShiroOYuki <63907529+ShiroOYuki@users.noreply.github.com>
2023-09-17 18:01:14 +0800

    update 09170601

--- ShiroTools/exam/views.py
+++ ShiroTools/exam/views.py
@@ -1,5 +1,38 @@
 from django.shortcuts import render
+import json
+import os
 
-# Create your views here.
-def index(request):
+def createRecents(request, resp_url, name, url, context={}):
+    if request.COOKIES.get("history"):
+        hist = json.loads(request.COOKIES["history"])
+    else:
+        hist = []
+    if name != "Dashboard" and (len(hist) == 0 or hist[0][0]) != name:
+        hist.insert(0, [name, url])
+    if len(hist) > 20:
+        hist = hist[:20]
+    context["history"] = hist
+    resp = render(
+        request, 
+        resp_url,
+        context=context
+    )
+    resp.set_cookie("history", json.dumps(hist))
+    return resp
+
+def index(request): 
+    return createRecents(request, "exam.html", "Shiro's Exam", "/exam")
+
+def question(request, qid="testa"):
+    read_question(qid)
     return render(request, "exam.html")
+
+def read_question(qid):
+    path = f"static/exam/questions/{qid}.json"
+    if os.path.exists(path):
+        with open(path, "r", encoding="utf-8") as jfile:
+            jdata = json.load(jfile)
+            questions = jdata["questions"]
+            answers = jdata["answers"]
+            return questions, answers
+    return False
